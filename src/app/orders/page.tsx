f8222d653de6799b7b1c1b91c2159a791bccd7f2
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-05-07 17:38:03 -0400

    feat: Add Orders path, add an API endpoint that returns the latest 5 ready orders.

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,13 @@
+import { Logo } from "@/components/ui";
+
+export default function OrdersPage() {
+    return (
+        <>
+            <h1 className="text-center mt-20 text-6xl font-black">Ordenes Listas</h1>
+
+            <Logo />
+
+            
+        </>
+    )
+}

51c9cf3188768babdd796d9976023e1f1d7340e2
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-05-07 21:39:11 -0400

    fix: Add dynamic force to API routes.

--- src/app/orders/api/route.ts
+++ src/app/orders/api/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const orders = await prisma.order.findMany({
         take: 5,
